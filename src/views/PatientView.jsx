8da918a36c56cd7cda29e4287abb2923bd449472
Fred Zabulon <fredzabulon133@gmail.com>
2024-01-24 17:44:43 +0300

    added a meetings section

--- src/views/PatientView.jsx
+++ src/views/PatientView.jsx
@@ -28,17 +28,20 @@ function PatientView({ openView, setOpenView, data, funct }) {
   const currentDate = new Date();
   return (
 <div className='grid place-items-center h-[100vh] top-0 left-0 right-0 z-[999] bg-[rgba(0,0,0,0.2)] absolute'>
-      <div className='w-[50%] h-[90%] rounded-md bg-[#F0F3F9]'>
-        <div className=' w-[95%] mt-4 mx-auto '>
+      <div className='w-[50%] h-[90%] relative overflow-y-scroll rounded-md bg-[#F0F3F9]'>
+
+        <div className="sticky bg-[#F0F3F9] z-50 top-0 py-4 left-0 right-0">
+          <div className=' w-[95%]  mx-auto '>
           <div className='flex justify-between items-center  '>
             <h1 className='font-bold text-xl'>View { funct}</h1>
             <div className='inline-block cursor-pointer' onClick={()=>{setOpenView(!openView)}}>
               <RxCross1 size={20} />
             </div>            
           </div>      
+          </div>      
         </div>
 
-
+        {/* Patient Detail section */}
         <hr className='mt-5 bg-slate-500' />
 
         <div className=' w-[95%] mt-4 mx-auto '>
@@ -62,8 +65,9 @@ function PatientView({ openView, setOpenView, data, funct }) {
           </div>    
 
         </div>   
-        
+        {/* Appointment section */}
         <hr className='mt-5 bg-slate-500' />
+
         <div className=' w-[95%] mt-4 mx-auto '>
           <div className="flex items-center space-x-4">
             <p className="text-textDark">Upcoming and Past Meetings</p>  
@@ -104,9 +108,37 @@ function PatientView({ openView, setOpenView, data, funct }) {
               }
 
               })}
-          </div>        
+          </div>  
+          
+          <div className="mt-5">
+            <p>Condition/Notes</p>
+          <p className="px-5 py-2 bg-white "> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore quo nihil ipsam, voluptates repellat ab perspiciatis nemo doloribus numquam ex, eaque modi. Quae, quo error? Similique iure voluptatum illo amet. </p>            
+          </div>
         </div>
 
+        {/* Prescription section */}
+        <hr className='mt-5 bg-slate-500' />
+        
+        <div className=' w-[95%] mt-4 mx-auto '>
+          <div className="flex justify-between">
+            <p className="text-textDark font-semibold">Prescription</p>
+            <p className="text-primary font-semibold cursor-pointer">Add Prescription</p>
+          </div>
+        </div>
+
+        {/* Laboratory Test section */}
+        <hr className='mt-5 bg-slate-500' />
+        
+        <div className=' w-[95%] mt-4 mx-auto '>
+
+        </div>     
+        
+        {/* Available Test section */}
+        <hr className='mt-5 bg-slate-500' />
+        
+        <div className=' w-[95%] mt-4 mx-auto '>
+
+        </div>        
       </div>
     </div>
   )
